677f21f27fedebe78dcafb19c793778fcdf2d6cc
rutvik110 <takrutvik@gmail.com>
2022-04-05 21:45:29 +0530

    live progress bar update

--- packages/youtube_player_flutter/lib/src/widgets/live_bottom_bar.dart
+++ packages/youtube_player_flutter/lib/src/widgets/live_bottom_bar.dart
@@ -2,12 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:youtube_player_flutter/src/utils/duration_notifier.dart';
 
 import '../utils/youtube_player_controller.dart';
+import 'duration_widgets.dart';
 import 'full_screen_button.dart';
 
 /// A widget to display bottom controls bar on Live Video Mode.
@@ -33,8 +31,6 @@ class LiveBottomBar extends StatefulWidget {
 }
 
 class _LiveBottomBarState extends State<LiveBottomBar> {
-  double maxValueForSlider = 0.0;
-
   late YoutubePlayerController _controller;
 
   @override
@@ -42,77 +38,72 @@ class _LiveBottomBarState extends State<LiveBottomBar> {
     // TODO: implement initState
     super.initState();
     _controller = widget.controller;
-    //   _controller.durationNotifier.addListener(listener);
   }
 
   @override
   Widget build(BuildContext context) {
-    return
-        //  !_controller.value.isControlsVisible
-        //     ? const SizedBox.shrink()
-        //     :
-        Row(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        const SizedBox(
-          width: 14.0,
-        ),
-        // CurrentPosition(
-        //   _controller,
-        // ),
-        Expanded(
-          child: Padding(
-            child: ValueListenableBuilder<VideoDurations>(
-                valueListenable: _controller.durationNotifier,
-                builder: (context, videoDueation, child) {
-                  if (maxValueForSlider <
-                      videoDueation.position.inMilliseconds) {
-                    maxValueForSlider =
-                        videoDueation.position.inMilliseconds.toDouble();
-                  }
-                  final sliderValue =
-                      videoDueation.position.inMilliseconds / maxValueForSlider;
-                  log(sliderValue.toString());
-                  return Slider(
-                    value: sliderValue.isNaN ? 1.0 : sliderValue,
-
-                    // min: 0,
-                    // max: maxValueForSlider,
-
-                    onChanged: (value) {
-                      _controller.seekTo(
-                        Duration(
-                          milliseconds: (maxValueForSlider * value).round(),
-                        ),
-                      );
-                    },
-                    activeColor: widget.liveUIColor,
-                    inactiveColor: Theme.of(context).colorScheme.secondary,
-                  );
-                }),
-            padding: const EdgeInsets.symmetric(
-              horizontal: 8.0,
+    return Visibility(
+      visible: _controller.value.isControlsVisible,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          const SizedBox(
+            width: 14.0,
+          ),
+          CurrentPosition(
+            _controller,
+          ),
+          Expanded(
+            child: Padding(
+              child: ValueListenableBuilder(
+                  valueListenable: _controller.durationNotifier,
+                  builder: (context, position, child) {
+                    final currentSliderPosition =
+                        _controller.metadata.duration.inMilliseconds == 0
+                            ? 0
+                            : _controller.durationNotifier.value.position
+                                    .inMilliseconds /
+                                _controller.metadata.duration.inMilliseconds;
+                    return Slider(
+                      value: currentSliderPosition.clamp(0, 1).toDouble(),
+                      onChanged: (value) {
+                        _controller.seekTo(
+                          Duration(
+                            milliseconds: (_controller.durationNotifier.value
+                                        .position.inMilliseconds *
+                                    value)
+                                .toInt(),
+                          ),
+                        );
+                      },
+                      activeColor: widget.liveUIColor,
+                      inactiveColor: Theme.of(context).colorScheme.secondary,
+                    );
+                  }),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 8.0,
+              ),
             ),
           ),
-        ),
-        // InkWell(
-        //   onTap: () => _controller.seekTo(_controller.metadata.duration),
-        //   child: Material(
-        //     color: widget.liveUIColor,
-        //     child: const Text(
-        //       ' LIVE ',
-        //       style: TextStyle(
-        //         color: Colors.white,
-        //         fontSize: 12.0,
-        //         fontWeight: FontWeight.w300,
-        //       ),
-        //     ),
-        //   ),
-        // ),
-        widget.showLiveFullscreenButton
-            ? FullScreenButton(controller: _controller)
-            : const SizedBox(width: 14.0),
-      ],
+          InkWell(
+            onTap: () => _controller.seekTo(_controller.metadata.duration),
+            child: Material(
+              color: widget.liveUIColor,
+              child: const Text(
+                ' LIVE ',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12.0,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ),
+          ),
+          widget.showLiveFullscreenButton
+              ? FullScreenButton(controller: _controller)
+              : const SizedBox(width: 14.0),
+        ],
+      ),
     );
   }
 }
